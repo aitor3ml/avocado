6f5f6a733b2f1d290a391eb9b9c76f2a6a59cc55
aitor3ml <aitor3ml@gmail.com>
2019-09-15 11:25:03 +0200

    avoid use of ObjectIOStream

--- AvocadoShared/src/main/java/com/aitor3ml/avocado/shared/networking/binary/BinaryCoder.java
+++ AvocadoShared/src/main/java/com/aitor3ml/avocado/shared/networking/binary/BinaryCoder.java
@@ -5,9 +5,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
 import java.nio.ByteBuffer;
 
 import com.aitor3ml.avocado.shared.networking.AvocadoDeserializer;
@@ -19,49 +16,16 @@ public class BinaryCoder implements WSProtocolConstants {
 
 	public static ByteBuffer encode(Message message) throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		if (message instanceof AvocadoSerializable) {
-			AvocadoSerializable as = (AvocadoSerializable) message;
-			baos.write(AVOCADO_SERIALIZABLE);
-			as.encode(new DataOutputStream(baos));
-		} else if (message instanceof Serializable) {
-			baos.write(JAVA_SERIALIZED);
-			try {
-				writeMessage(baos, message);
-			} catch (IOException e) {
-				e.printStackTrace();
-				throw new RuntimeException(e);
-			}
-		} else
-			throw new RuntimeException("don't know to encode " + message.toString());
+		AvocadoSerializable as = (AvocadoSerializable) message;
+		as.encode(new DataOutputStream(baos));
 		return ByteBuffer.wrap(baos.toByteArray());
 	}
 
 	public static Message decode(InputStream stream, AvocadoDeserializer deserializer)
 			throws IOException, ClassNotFoundException {
-		int code = stream.read();
-		switch (code) {
-		case AVOCADO_SERIALIZABLE:
-			if (deserializer == null)
-				throw new RuntimeException("no AvocadoDeserializer provided");
-			return deserializer.deserialize(new DataInputStream(stream));
-		case JAVA_SERIALIZED:
-			return readMessage(stream);
-		default:
-			throw new RuntimeException("don't know to decode " + code);
-		}
-	}
-
-	public static Message readMessage(InputStream stream) throws IOException, ClassNotFoundException {
-		ObjectInputStream in = new ObjectInputStream(stream);
-		Message m = (Message) in.readObject();
-		in.close();
-		return m;
-	}
-
-	public static void writeMessage(ByteArrayOutputStream baos, Message message) throws IOException {
-		ObjectOutputStream out = new ObjectOutputStream(baos);
-		out.writeObject(message);
-		out.close();
+		if (deserializer == null)
+			throw new RuntimeException("no AvocadoDeserializer provided");
+		return deserializer.deserialize(new DataInputStream(stream));
 	}
 
 }
--- AvocadoShared/src/main/java/com/aitor3ml/avocado/shared/networking/websocket/WSProtocolConstants.java
+++ AvocadoShared/src/main/java/com/aitor3ml/avocado/shared/networking/websocket/WSProtocolConstants.java
@@ -2,8 +2,4 @@ package com.aitor3ml.avocado.shared.networking.websocket;
 
 public interface WSProtocolConstants {
 
-	public static final int AVOCADO_SERIALIZABLE = 1;
-
-	public static final int JAVA_SERIALIZED = 2;
-
 }
